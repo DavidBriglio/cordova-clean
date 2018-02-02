96f8a4be747ef56f6a4fdd0f18cb0e76767f8f94
David Briglio <david.briglio@versaterm.com>
2018-02-02 13:52:48 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # cordova-cleanup-plugins
-Removes installed plugins and installs the plugins listed in the config.xml file.
+This is a nuke approach to the issue of inconsistent cordova plugins of a project when working in different branches. It can remove all installed plugins and install all plugins listed in the config.xml file. This avoids the case of having left over plugins from other branches installed, and if a plugin has been updated without updating the version number.
+
+## How To Install
+Install through NPM: (global optional)
+```
+npm install -g cordova-cleanup-plugins
+```
+**This package depends on any version of cordova being installed.**
+
+## How To Use 
+Run the shell command: `cordova-clean <OPTIONS>`
+
+All options are optional:
+
+| Option | Short | Explanation |
+|---|---|---|
+| `--noremove` | `--nr` | Prevents the remove all plugins step from running. Use this if you only want to install the config.xml plugins on top of the installed plugins. |
+| `--noforce` | `--nf` | Prevents force uninstall plugins. By default all plugins are removed using the `--force` flag, this way all plugins will be removed even if they are dependencies to other plugins. |
+| `--gitfetch` | `--gf` | Prevents `--nofetch` from being used when installing git repository plugins. This is mostly an issue with cordova versions below 8, it will fail installing plugins from git repositories because it attempts to add them as an npm package instead of using git. By default nofetch is used to prevent this issue from happening. |
+
+
+# MIT License
+Please see the LICENSE.md file for more information on licensing.
---
+++ index.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var cmd = require('child_process');
+var noForce = (process.argv.indexOf("--noforce") >= 0 || process.argv.indexOf("--nf") >= 0);
+var noRemove = (process.argv.indexOf("--noremove") >= 0 || process.argv.indexOf("--nr") >= 0);
+var fetch = (process.argv.indexOf("--gitfetch") >= 0 || process.argv.indexOf("--gf") >= 0);
+var content;
+
+console.log("\n================<Cleaning Cordova Plugins>================");
+
+if (noForce || noRemove) {
+    console.log("\n===> Arguments Found:" + (noRemove ? " <NO REMOVE>" : "") + (noForce ? " <NO FORCE>" : "") + (fetch ? " <GIT FETCH>" : ""));
+}
+
+if (!fs.existsSync(process.cwd() + "/config.xml")) {
+    console.error("\n==> ERROR: No config.xml found!");
+    console.log("\n==========================<Done>==========================\n");
+    return;
+}
+
+try {
+    content = fs.readFileSync(process.cwd() + "/config.xml");
+} catch (e) {
+    // Do nothing, this will be handled by logic below
+}
+
+if (!fs.existsSync(process.cwd() + "/config.xml")) {
+    console.error("\n==> ERROR: Problem reading config.xml!");
+    console.log("\n==========================<Done>==========================\n");
+    return;
+}
+
+console.log("\n=================<Finding Config Plugins>=================");
+var pattern = /<plugin[ ]+name=\"(.+)\"[ ]+spec=\"(.+)\"/gi;
+var match = pattern.exec(content);
+var plugins = {};
+while (match !== null) {
+    console.log("FOUND " + match[1] + " @ " + match[2]);
+    plugins[match[1]] = match[2].replace("git+", ""); //.replace("^", "").replace("~", "");
+    match = pattern.exec(content);
+}
+
+if (noRemove) {
+    console.log("\n=====================<Skipped Remove>=====================");
+} else {
+    console.log("\n================<Finding Installed Plugins>===============");
+    var foundItems = fs.readdirSync(process.cwd() + "/plugins/");
+    var iplugins = [];
+    for (var index in foundItems) {
+        if (fs.lstatSync(process.cwd() + "/plugins/" + foundItems[index]).isDirectory()) {
+            console.log("FOUND " + foundItems[index]);
+            iplugins.push(foundItems[index]);
+        }
+    }
+
+    console.log("\n===============<Removing Installed Plugins>===============");
+    for (var index in iplugins) {
+        console.log("\n===> Removing " + iplugins[index]);
+        try {
+            cmd.execSync("cordova plugin rm " + iplugins[index] + " --nosave" + (noForce ? "" : " --force"), {
+                stdio: [0, 1, 2]
+            });
+        } catch (e) {
+            // Do nothing, we do not want to stop if there was an error removing a plugin
+        }
+    }
+}
+
+
+console.log("\n==============<Installing Config.xml Plugins>=============");
+for (var plugin in plugins) {
+    console.log("\n===> Installing " + plugin + " @ " + plugins[plugin]);
+    if (plugins[plugin].match("git+")) {
+        try {
+            cmd.execSync("cordova plugin add " + plugins[plugin] + (fetch ? "" : " --nofetch ") + " --nosave", {
+                stdio: [0, 1, 2]
+            });
+        } catch (e) {
+            // Do nothing, we do not want to stop if there was an error installing the plugin
+        }
+    } else {
+        try {
+            cmd.execSync("cordova plugin add " + plugin + "@" + plugins[plugin] + " --nosave", {
+                stdio: [0, 1, 2]
+            });
+        } catch (e) {
+            // Do nothing, we do not want to stop if there was an error installing the plugin
+        }
+    }
+}
+
+console.log("\n==========================<Done>==========================\n");
